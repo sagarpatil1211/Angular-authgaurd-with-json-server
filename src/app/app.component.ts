052ea9a2eebded19cee2bccfec38711bfba1ce1d
Sagar <patilsagar1211@gmail.com>
2023-02-03 17:53:26 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '13.1_auth_guard_with_mockAPI';
+}
